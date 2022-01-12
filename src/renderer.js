c49cc07408391bfd8e8f1e2d390ff5457bc51cca
Stephen Spoon <sspoon.itt@gmail.com>
2022-01-12 16:04:54 -0500

    Minor tweaks to formatting

--- src/renderer.js
+++ src/renderer.js
@@ -8,16 +8,20 @@ function getClock() {
 
 	if(hours > 12) {
 		clockHours.innerText = hours - 12;
-		clockNoons.innerText = "pm";
 	} else {
 		clockHours.innerText = hours;
-		clockNoons.innerText = "am";
 	}
 
 	if(minutes < 10) {
 		clockMinutes.innerText = "0" + minutes;
 	} else {
 		clockMinutes.innerText = minutes;
+	}
+
+	if(hours >= 12) {
+		clockNoons.innerText = "pm";
+	} else {
+		clockNoons.innerText = "am";
 	}
 }
 
